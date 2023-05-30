7557be2ddc3b338c5ff89e76af192752217c4583
kristina-lim <kristinahlim@gmail.com>
2023-05-30 16:26:32 -0700

    Install material ui and add icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
 import React from "react";
 import './Header.css';
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Header() {
   return (
-    <div className='Header'>
+    <div className="header">
       <img 
         className="header__icon"
         src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
         alt=""
       />
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
     </div>
   )
 }
