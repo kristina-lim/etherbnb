84595b3bf975715f7e17950219118a07f4714aae
kristina-lim <kristinahlim@gmail.com>
2023-06-16 12:05:34 -0700

    Add Banner component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Home.css';
+import Banner from '../Banner/Banner';
 
 export default function Home() {
   return (
     <div className="home">
-      
+      <Banner />
     </div>
   )
 }
