73685814ac9b7602385d56105603463c0172ff71
kristina-lim <kristinahlim@gmail.com>
2023-06-21 15:48:38 -0700

    Change icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
     <div className='header'>
       <img 
         className='header__icon'
-        src='https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png'
+        src='https://i.imgur.com/UFuuR65.png'
         alt=''
       />
       <div className='header__center'>
