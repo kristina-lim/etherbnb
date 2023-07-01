8b46b33fdccff680039cc3fc233f7f01f53772a7
kristina-lim <kristinahlim@gmail.com>
2023-06-30 17:10:46 -0700

    Add card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,10 @@
+import React from 'React';
+import './Card.css';
+
+export default function Card({ src, title, description, price }) {
+  return (
+    <div className='card'>
+      
+    </div>
+  )
+}
