ab7325b05bf43d4ae26aef47c764624ffbbac94d
kristina-lim <kristinahlim@gmail.com>
2023-08-29 17:31:05 -0700

    import Footer component in app and info in footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className='footer'>
-      
+      <p>&#169; 2023 Etherbnb, Airbnb clone. No rights reserved - this is a clone!</p>
+      <p>Privacy • Terms • Sitemap • Company Details</p>
     </div>
   )
 }
