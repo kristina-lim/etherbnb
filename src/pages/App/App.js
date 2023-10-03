07489f048386380da5b8740a92ddf24b773c8ce6
kristina-lim <kristinahlim@gmail.com>
2023-10-03 15:36:20 -0700

    Install react router and import in app

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import Home from '../../pages/Home/Home';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App() {
   return (
